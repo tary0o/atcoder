884b258b949790df3f369b01eb7ddfb19b716d98
tary0o <taryo.teri@gmail.com>
2022-06-13 16:54:20 +0900

    first commit

--- ABC/255D.py
+++ ABC/255D.py
@@ -1,13 +1,28 @@
-import numpy as np
 N,Q = map(int,input().split())
-A = np.array(list(map(int,input().split())))
+A = [0] +list(map(int,input().split()))
+A.sort()
 ans = []
-sumA = sum(A)
+sumA = [A[0]]
+
+for i in range(1,N+1):
+    sumA.append(sumA[i-1]+A[i])
 
 for _ in range(Q):
     X = int(input())
-    B = abs(np.array([X]*N)-A)
-    ans.append(np.sum(B))
+    ok = 0
+    ng = N+1
+    while ng-ok>1:
+        chk = (ok+ng)//2
+        if A[chk]==X:
+            ok = chk
+            break
+        elif A[chk]<X:
+            ok = chk
+        elif A[chk]>X:
+            ng = chk
+    
+    B = -2*sumA[ok]+sumA[N]+(ok)*X-(N-ok)*X 
+    ans.append(B)
 
 for a in ans:
     print(a)
