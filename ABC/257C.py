da3063bba5998a1a1b567e2944f117950c4f5f04
tary0o <taryo.teri@gmail.com>
2022-07-02 16:01:12 +0900

    first commit

--- ABC/257C.py
+++ ABC/257C.py
@@ -9,15 +9,40 @@ for i in range(N):
     l.append([W[i],S[i]])
 
 sorted_l = sorted(l, reverse=False, key=lambda x:x[0])
-print(sorted_l)
 
+tmpans = adult
 ans = adult
-bf = sorted_l[0][1]
-cnt = 0
-for i in range(1,N):
-    if bf == sorted_l[i,1]:
-        cnt += 1
-        continue
+cnt = [0,0]
+for i in range(1,N+1):
+    if i != N:
+        if sorted_l[i-1][0] == sorted_l[i][0]:
+            cnt[0] += 1
+            if sorted_l[i-1][1]=='0':
+                cnt[1] += 1
+        else:
+            if cnt[0]==0:
+                if sorted_l[i-1][1]=='0':
+                    tmpans += 1
+                else:
+                    tmpans -= 1
+            else:
+                cnt[0] += 1
+                if sorted_l[i-1][1]=='0':
+                    cnt[1] += 1
+                tmpans += (-cnt[0]+2*cnt[1])
+                cnt = [0,0]
     else:
-        cnt = 0
+        if cnt[0] == 0:
+            if sorted_l[i-1][1]=='0':
+                tmpans += 1
+            else:
+                tmpans -= 1
+        else:
+            if sorted_l[i-1][1]=='0':
+                cnt[1] += 1
+            cnt[0] += 1
+            tmpans += (-cnt[0]+2*cnt[1])
+    ans = max(ans,tmpans)
+
+print(ans)
         
