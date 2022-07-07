69d685fbb71f4cf1f193e7a37ec4d1f726b8ddf8
tary0o <taryo.teri@gmail.com>
2022-07-07 15:28:30 +0900

    first commit

--- アルゴリズムと数学/test.py
+++ アルゴリズムと数学/test.py
@@ -1,39 +1,15 @@
-import heapq
+def multiply(A,B):
+	n = len(A)
+	print(n)
+	C = [[0]*n for _ in range(n)]
+	for i in range(n):
+		for j in range(n):
+			for k in range(n):
+				C[i][j] += A[i][k]*B[k][j]
+	return C
 
-# 入力
-K = int(input())
+A = [[1,1,1],[2,0,1],[2,1,1]]
+B = [[2,2,1],[3,0,1],[1,1,1]]
+C = multiply(A,B)
 
-# 隣接リストの作成 → グラフの辺を追加
-G = [ list() for i in range(K) ]
-for i in range(K):
-	for j in range(10):
-		if i == 0 and j == 0:
-			continue
-		G[i].append(((i * 10 + j) % K, j))
-print(G[2])
-
-# ダイクストラ法：配列の初期化など
-dist = [ 10 ** 10 ] * K
-used = [ False ] * K
-Q = list()
-heapq.heappush(Q, (5, 100))  # ここでは dist[0] = 0 にはしないことに注意！
-
-
-# ダイクストラ法：優先度付きキューの更新
-while len(Q) >= 1:
-	pos = heapq.heappop(Q)[1]
-	if used[pos] == True:
-		continue
-	used[pos] = True
-	for i in G[pos]:
-		to = i[0]
-		cost = dist[pos] + i[1]
-		if pos == 0:
-			cost = i[1]  # 頂点 0 の場合は例外
-		if dist[to] > cost:
-			dist[to] = cost
-			heapq.heappush(Q, (dist[to], to))
-
-# 答えを出力
-print(dist)
-print(dist[0])
+print(C)
